6c2c38a19ce67b10b8f81fbd10b7a8acb692462e
Salah Ahmed <salah.ahmed.hslc@gmail.com>
2016-06-17 05:44:59 -0400

    print to standard output datetime + text with html tags

---
+++ pr_logger.py
@@ -0,0 +1,14 @@
+import sys
+from datetime import datetime
+
+
+def pr_logger(text):
+    timestamp = datetime.now()
+    html = """<h2>{0}</h2><code>{1}</code>""".format(timestamp, text)
+    return html + '\n\n'
+
+
+if __name__ == "__main__":
+    log = "logger.md"
+    lines = sys.argv[1]
+    print(pr_logger(lines))
