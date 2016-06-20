29751d0eb8b3fa3d56153a2a2a4a59422cf58633
Salah Ahmed <salah.ahmed.hslc@gmail.com>
2016-06-20 14:21:26 -0400

    use argument parser

--- pr_logger.py
+++ pr_logger.py
@@ -1,14 +1,27 @@
 #! /usr/local/bin/python
-import sys
+from argparse import ArgumentParser
 from datetime import datetime
 
 
-def pr_logger(text):
-    timestamp = datetime.now()
-    html = """<h2>{0}</h2><p>{1}</p>""".format(timestamp, text)
-    return html + '\n\n'
+def pr_logger(text, htag='h4', btag='p'):
+    return make_template(datetime.now(), text, htag, btag)
+
+
+def make_template(header, body, htag='h1', btag='p'):
+    return "<{htag}>{header}</{htag}><{btag}>{body}</{btag}>\n\n".format(
+                header=header, body=body, htag=htag, btag=btag)
 
 
 if __name__ == "__main__":
-    lines = sys.argv[1]
-    print(pr_logger(lines))
+    parser = ArgumentParser(
+        description='Log updates with current time')
+    parser.add_argument('content', metavar='CONTENT')
+    parser.add_argument(
+        '-t', metavar='HEAD_TAG', nargs='?',
+        choices=['h1', 'h2', 'h3', 'h4'],
+        help='the header tag used')
+    parser.add_argument(
+        '-b', metavar='BODY_TAG', nargs='?',
+        help='the tag of the content used')
+    args = parser.parse_args()
+    print(pr_logger(args.content, args.t, args.b))
